7867ba81593e4a32a46c45da3f5cce647a7c35fb
Dani Ortiz <danielorrami@hotmail.com>
2024-11-25 17:15:43 -0300

    Update EditProfileActivity.java

--- app/src/main/java/com/danior/loginfinal/EditProfileActivity.java
+++ app/src/main/java/com/danior/loginfinal/EditProfileActivity.java
@@ -25,8 +25,6 @@ public class EditProfileActivity extends AppCompatActivity {
 
         // Acción para el botón de guardar cambios
         saveButton.setOnClickListener(v -> {
-            // Aquí puedes agregar la lógica para guardar los cambios (como en una base de datos o SharedPreferences)
-            // Simularemos que los cambios se guardan y se vuelve a la pantalla de perfil
             finish(); // Esto cierra la actividad actual (EditProfileActivity) y regresa a la anterior (ProfileActivity)
         });
     }
